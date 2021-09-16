d91afdb504752459f8600c8cf8d3697c917f5816
phillip yang <phillipyang1179@gmail.com>
2021-09-15 21:02:46 -0700

    change

--- src/App.js
+++ src/App.js
@@ -1,20 +1,5 @@
-// import "./App.css";
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <p>Hello CSCI E-39!</p>
-//         <p>My name is Phillip Yang</p>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-import logo from "./logo.svg";
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
